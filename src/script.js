a454d3d3848e2db8629cfb0ec53fc68c25eb3e91
Max <masamune.code@gmail.com>
2020-09-17 18:55:39 +0200

    REF: JS > generate summary links

--- src/script.js
+++ src/script.js
@@ -11,11 +11,12 @@ gsap.registerPlugin(ScrollToPlugin);
 import { bottomButtons }      from './assets/js/bottomButtons.js';
 import { manageFonts }        from './assets/js/fonts.js';
 // import { responsiveViaJs } from './assets/js/responsiveViaJs.js';
-import { bottomButtons }      from './assets/js/bottomButtons.js';
+import { summary }            from './assets/js/summary.js';
 
 
 window.addEventListener('DOMContentLoaded', () => {
   
+  // Classic stuff : fonts & responsive
   manageFonts();
   // responsiveViaJs();
   
@@ -26,30 +27,9 @@ window.addEventListener('DOMContentLoaded', () => {
   bottomButtons(sectionsHTML);
   
   /// Génération des liens du sommaire
-  // On vire la page sommaire des sections
-  const cloneSectionsPourSommaire = sectionsHTML.slice(1);
+  summary(sectionsHTML);
 
-  // Récupération de la liste du sommaire
-  let ulHTML = document.querySelector('#sommaire ul');
-
-  for(let i = 0 ; i < cloneSectionsPourSommaire.length ; i++ ) {
-    const texteDuH2DeLaSection = cloneSectionsPourSommaire[i].querySelector('h2').textContent;
-
-    // Création de l'élément de liste qui contient un lien
-    const newListItem = document.createElement('li');
-    const newLink     = document.createElement('a');
-    const newContent  = document.createTextNode(texteDuH2DeLaSection);
-
-    // Imbrication des éléments puis ajout au html, dans la liste
-    newLink     .appendChild(newContent);
-    newListItem .appendChild(newLink);
-    ulHTML      .appendChild(newListItem);
-
-    // Ajout de l'animation au clic (scroll vers la section concernée)
-    newLink.addEventListener('click', () => {
-      gsap.to(window, {duration: 1, scrollTo: '#' + cloneSectionsPourSommaire[i].id});
-    });
-  }
+  
 
   //// Gestion des couleurs
   let couleursHTML = gsap.utils.toArray('.colors .color');
