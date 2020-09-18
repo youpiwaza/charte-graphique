2e71d70f549662d5440dd92d311fb8e4cec4d636
Max <masamune.code@gmail.com>
2020-09-18 13:36:37 +0200

    ADD: Content : fonts & visual aspect

--- src/assets/js/fonts.js
+++ src/assets/js/fonts.js
@@ -5,10 +5,23 @@ export function manageFonts() {
   
   //// Font face observer
   // https://github.com/bramstein/fontfaceobserver
-  //                                family name declared in css @import
-  const font = new FontFaceObserver('Roboto Condensed');
-  font.load().then( () => {
+  //    family name declared in css @import
+  //    Prefer loading each font individually
+  const font1 = new FontFaceObserver('Shadows Into Light');
+  font1.load().then( () => {
     const bodyHTML = document.getElementsByTagName('body')[0];
-    bodyHTML.classList.add('fonts-loaded');
+    bodyHTML.classList.add('font1-loaded');
+  });
+
+  const font2 = new FontFaceObserver('Roboto Condensed');
+  font2.load().then( () => {
+    const bodyHTML = document.getElementsByTagName('body')[0];
+    bodyHTML.classList.add('font2-loaded');
+  });
+  
+  const font3 = new FontFaceObserver('Press Start 2P');
+  font3.load().then( () => {
+    const bodyHTML = document.getElementsByTagName('body')[0];
+    bodyHTML.classList.add('font2-loaded');
   });
 }
